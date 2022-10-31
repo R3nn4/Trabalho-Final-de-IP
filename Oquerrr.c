8b34be399bfe23dd64bf6b874c7d0c120f7941c4
Luiz Henrique <maestrellipeixoto@gmail.com>
2022-10-31 20:17:41 -0300

    main

---
+++ Oquerrr.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Pessoas.h"
+#include "Grupos.h"
+#include "funcs_uteis.h"
+#define SUCESSO 0
+
+int main(int argc, char ** argv)
+{
+    FILE *pessoas = fopen("Pessoas.txt", "r");
+    FILE *grupos = fopen("Grupos.txt", "r");
+    FILE *conversas = fopen("Conversas.txt", "r");
+    int choice, n_pessoas = 0, n_grupos = 0;
+    Pessoa *p = NULL;
+    Grupo *g = NULL;
+    Conversas *c = NULL;
+        cria_arquivos(pessoas, grupos, conversas);
+        do{
+            printf("\n\t\tOquerrr\n");
+            imprime_traco();
+            printf("\t1 - Pessoas\n\n\t2 - Grupos\n\n\t3 - Conversas\n\n\t4 - Sair do App\n> ");
+            scanf("%d%*c", &choice);
+            switch (choice)
+            {
+            case 4:                             //Voltar
+                break;
+            case 1:                             //Módulo de Pessoas
+                p = mod_pessoa(p, &n_pessoas);  //passo como pointer para modificar em todo o programa
+                break;
+            case 2:                             //Módulo de Grupos
+                g = mod_grupo(g, p, &n_grupos, n_pessoas);
+                
+                break;  
+            case 3:
+                /*code*/                //Módulo de Conversas
+                break; 
+            default:                    //Opção inválida
+                break;
+            }
+        }while(choice != 4);
+        free(p);
+        free(g);
+        printf("Backup feito com sucesso\n");
+        printf("Saindo...");
+    return SUCESSO;
+}
